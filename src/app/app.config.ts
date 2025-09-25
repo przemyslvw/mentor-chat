2678aca5f402966613948aa7792d5aea9c2af727
Przemas <przemyslvw@gmail.com>
2025-09-25 17:27:26 +0200

    angular/material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"zapytaj-6fbcc","appId":"1:165410793587:web:90820ac245356f09e56206","storageBucket":"zapytaj-6fbcc.firebasestorage.app","apiKey":"AIzaSyBN2AuxwXevQrpj4xTTgM5WELD872OgGTA","authDomain":"zapytaj-6fbcc.firebaseapp.com","messagingSenderId":"165410793587","measurementId":"G-KQGKPRB6PJ"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"zapytaj-6fbcc","appId":"1:165410793587:web:90820ac245356f09e56206","storageBucket":"zapytaj-6fbcc.firebasestorage.app","apiKey":"AIzaSyBN2AuxwXevQrpj4xTTgM5WELD872OgGTA","authDomain":"zapytaj-6fbcc.firebaseapp.com","messagingSenderId":"165410793587","measurementId":"G-KQGKPRB6PJ"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideAnimationsAsync()]
 };
