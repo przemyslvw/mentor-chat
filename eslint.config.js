bc29c34d3b0fccf42c7d45e5d3f94ee1707dc7ad
Przemas <przemyslvw@gmail.com>
2025-09-26 17:23:49 +0200

    Konfiguracja ESLint, Prettier, pre-commit hooks

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      {
+        plugins: {
+          prettier: require('eslint-plugin-prettier'),
+        },
+        rules: {
+          ...prettierConfig.rules,
+          'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        },
+      },
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      // Additional rules
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {
+      // Add any HTML-specific rules here
+    },
+  },
+);
