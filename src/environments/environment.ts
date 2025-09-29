e5f00c649ef1867cad36dc3baa958664a05ccd37
Przemas <przemyslvw@gmail.com>
2025-09-29 18:19:30 +0200

    gemini mockkey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
-    production: false,
-    firebase: {
-      apiKey: 'AIza...devApiKey',
-      authDomain: 'your-project-dev.firebaseapp.com',
-      projectId: 'your-project-dev',
-      storageBucket: 'your-project-dev.appspot.com',
-      messagingSenderId: '1234567890',
-      appId: '1:1234567890:web:devAppId'
-    },
-    openAIKey: 'sk-dev-your-openai-key',
-    perplexityKey: 'pplx-dev-your-perplexity-key'
-  };
-  
+  production: false,
+  firebase: {
+    apiKey: 'AIza...devApiKey',
+    authDomain: 'your-project-dev.firebaseapp.com',
+    projectId: 'your-project-dev',
+    storageBucket: 'your-project-dev.appspot.com',
+    messagingSenderId: '1234567890',
+    appId: '1:1234567890:web:devAppId',
+  },
+  openAIKey: 'sk-dev-your-openai-key',
+  perplexityKey: 'pplx-dev-your-perplexity-key',
+  geminiAIKey: 'gemini--dev-your-gemini-key',
+};
